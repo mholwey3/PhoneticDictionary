57920b09203ea95f711d1ead80137774cca29cfc
Michael Holwey <mholwey3@hotmail.com>
2019-05-09 00:00:26 -0500

    Added script for loading dictionary content. Added a structure to hold a dictionary item. Added the BASE A csv file.

---
+++ Assets/Scripts/PhoneticDictionary.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+
+public class PhoneticDictionary : MonoBehaviour
+{
+	private const string _FILE_PATH = "./Assets/Data/PHONETIC_DICTIONARY_BASE_A.csv";
+	private List<PhoneticDictionaryItem> _list_DictionaryItems;
+
+	private void Awake()
+	{
+		LoadData();
+	}
+
+	private void LoadData()
+	{
+		StreamReader reader = new StreamReader(File.OpenRead(_FILE_PATH));
+		_list_DictionaryItems = new List<PhoneticDictionaryItem>();
+		while (!reader.EndOfStream)
+		{
+			string[] sLineComponents = reader.ReadLine().Split(',');
+			PhoneticDictionaryItem item = new PhoneticDictionaryItem(sLineComponents[0], sLineComponents[1], sLineComponents[2]);
+			_list_DictionaryItems.Add(item);
+		}
+	}
+}
---
+++ Assets/Scripts/PhoneticDictionaryItem.cs
@@ -0,0 +1,36 @@
+﻿public class PhoneticDictionaryItem
+{
+	private string _sWord;
+	public string Word
+	{
+		get
+		{
+			return _sWord;
+		}
+	}
+
+	private string _sDictionaryPronunciation;
+	public string DictionaryPronuncitation
+	{
+		get
+		{
+			return _sDictionaryPronunciation;
+		}
+	}
+
+	private string _sPhoneticSpelling;
+	public string PhoneticSpelling
+	{
+		get
+		{
+			return _sPhoneticSpelling;
+		}
+	}
+
+	public PhoneticDictionaryItem(string sWord, string sDictionaryPronunciation, string sPhoneticSpelling)
+	{
+		_sWord = sWord;
+		_sDictionaryPronunciation = sDictionaryPronunciation;
+		_sPhoneticSpelling = sPhoneticSpelling;
+	}
+}
